2591ed905bbe9c000317abd8291a8cea70f3a921
Akira Yamamoto <ak-yamamoto@k-bokka.com>
2021-03-13 21:58:08 +0900

    Add factorial func with type

--- chap_2/baby2.hs
+++ chap_2/baby2.hs
@@ -1,4 +1,8 @@
 removeNonUppercase :: [Char] -> [Char]
 removeNonUppercase st = [ c | c <- st, elem c ['A'..'Z'] ]
+
 addThree :: Int -> Int -> Int -> Int
 addThree x y z = x + y + z
+
+factorial :: Integer -> Integer
+factorial n = product [1..n]
