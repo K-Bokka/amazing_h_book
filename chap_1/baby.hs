b14d21260650318c10efda280395c51e13f867be
Akira Yamamoto <ak-yamamoto@k-bokka.com>
2021-03-13 21:47:58 +0900

    Add right triagles func

--- chap_1/baby.hs
+++ chap_1/baby.hs
@@ -8,3 +8,8 @@ conanO'Brien = "It's a-me, Conan O'Brien"
 boomBangs xs = [ if x < 10 then "BOOM!" else "BANG!" | x <- xs, odd x ]
 length' xs = sum [ 1 | _ <- xs ]
 removeNonUppercase st = [ c | c <- st, elem c ['A'..'Z'] ]
+rightTriangles = [
+    (a, b, c) |
+    c <- [1..10], a <- [1..c], b <- [1..a],
+    a^2 + b^2 == c^2, a + b + c == 24
+  ]
