bc43a49dfa6b52df224f8c22c0dee7b9321a8ce9
Akira Yamamoto <ak-yamamoto@k-bokka.com>
2021-03-13 22:26:19 +0900

    Add sayMe func

--- chap_3/baby3.hs
+++ chap_3/baby3.hs
@@ -1,3 +1,11 @@
 lucky :: Int -> String
 lucky 7 = "Lucky Number Seven!"
 lucky x = "Sorry, you're out of luck"
+
+sayMe :: Int -> String
+sayMe 1 = "One!"
+sayMe 2 = "Two!"
+sayMe 3 = "Three!"
+sayMe 4 = "Four!"
+sayMe 5 = "Five!"
+sayMe x = "Not between 1 and 5"
