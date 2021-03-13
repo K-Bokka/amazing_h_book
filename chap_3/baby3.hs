fc300109dc8c9cee3a19fbcbf46ac4802551c59f
Akira Yamamoto <ak-yamamoto@k-bokka.com>
2021-03-13 22:38:01 +0900

    Add head' func

--- chap_3/baby3.hs
+++ chap_3/baby3.hs
@@ -14,3 +14,7 @@ sayMe x = "Not between 1 and 5. Input Number is " ++ show x
 factorial :: Int -> Int
 factorial 0 = 1
 factorial n = n * factorial (n -1)
+
+head' :: [a] -> a
+head' [] = error "Can't call head on an empty"
+head' (x:_) = x
