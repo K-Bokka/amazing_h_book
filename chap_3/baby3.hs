3cd7abab2cd123c1a1516202222bdc37a33bf87b
Akira Yamamoto <ak-yamamoto@k-bokka.com>
2021-03-13 22:33:13 +0900

    Add -Wall -Werror options

--- chap_3/baby3.hs
+++ chap_3/baby3.hs
@@ -1,6 +1,7 @@
+{-# OPTIONS -Wall -Werror #-}
 lucky :: Int -> String
 lucky 7 = "Lucky Number Seven!"
-lucky x = "Sorry, you're out of luck"
+lucky _ = "Sorry, you're out of luck"
 
 sayMe :: Int -> String
 sayMe 1 = "One!"
@@ -8,7 +9,7 @@ sayMe 2 = "Two!"
 sayMe 3 = "Three!"
 sayMe 4 = "Four!"
 sayMe 5 = "Five!"
-sayMe x = "Not between 1 and 5"
+sayMe x = "Not between 1 and 5. Input Number is " ++ show x
 
 factorial :: Int -> Int
 factorial 0 = 1
