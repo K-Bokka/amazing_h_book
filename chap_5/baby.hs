f62c33e20165693ad91c45bc8c65629cdb16a6ba
Akira Yamamoto <ak-yamamoto@k-bokka.com>
2021-03-14 16:31:56 +0900

    Add numLongChain func

--- chap_5/baby.hs
+++ chap_5/baby.hs
@@ -44,3 +44,7 @@ chain n
     | even n = n : chain (div n 2)
     | odd n  = n : chain (n * 3 + 1)
     | otherwise = error "This integer is not even or odd. what???"
+
+numLongChain :: Int
+numLongChain = length (filter isLong (map chain [1..100]))
+    where isLong xs = length xs > 15
