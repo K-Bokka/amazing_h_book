5d88547376b119425d3bf7cce0ca176b395cfa05
Akira Yamamoto <ak-yamamoto@k-bokka.com>
2021-03-14 15:41:04 +0900

    Add multiTwoWithNine' func

--- chap_5/baby.hs
+++ chap_5/baby.hs
@@ -5,3 +5,6 @@ multiThree x y z = x * y * z
 
 multiThree' :: Int -> (Int -> (Int -> Int))
 multiThree' x y z = x * y * z
+
+multiTwoWithNine :: Int -> (Int -> Int)
+multiTwoWithNine = multiThree' 9
