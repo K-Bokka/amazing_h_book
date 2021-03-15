afdcf9273c89997dc574f66ead63a86eba0335f1
Akira Yamamoto <ak-yamamoto@k-bokka.com>
2021-03-16 00:40:34 +0900

    Add stringToDigit func

--- chap_6/baby.hs
+++ chap_6/baby.hs
@@ -47,3 +47,5 @@ findKey' key ((k, v):xs)
 findKey'' :: (Eq k) => k -> [(k, v)] -> Maybe v
 findKey'' key xs = foldr (\(k, v) acc -> if key == k then Just v else acc) Nothing xs
 
+stringToDigit :: String -> [Int]
+stringToDigit = map digitToInt . filter isDigit
