7d1ab66ca85d6b37f41c1163a6476886ff4a866e
Akira Yamamoto <ak-yamamoto@k-bokka.com>
2021-03-15 23:50:30 +0900

    Qualified import Data.Map.Strict

--- chap_6/baby.hs
+++ chap_6/baby.hs
@@ -1,6 +1,13 @@
 {-# OPTIONS -Wall -Werror #-}
 
 import Data.List hiding (sort)
+import qualified Data.Map.Strict as M
 
 numUniques :: (Eq a) => [a] -> Int
 numUniques = length . nub
+
+fruits::[(String,Int)]
+fruits = [("apple",1),("orange",2),("banana",3),("peach",4),("cherry",5),("orange",6),("apple",7),("peach",8)]
+
+fruitsMap :: M.Map String Int
+fruitsMap = M.fromList fruits
