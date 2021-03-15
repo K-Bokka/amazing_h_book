91910cc039a369871850fb34c7f09631cdab73c1
Akira Yamamoto <ak-yamamoto@k-bokka.com>
2021-03-16 00:29:14 +0900

    Add findKey'' func

--- chap_6/baby.hs
+++ chap_6/baby.hs
@@ -46,3 +46,6 @@ findKey' _ [] = Nothing
 findKey' key ((k, v):xs)
     | key == k = Just v
     | otherwise = findKey' key xs
+
+findKey'' :: (Eq k) => k -> [(k, v)] -> Maybe v
+findKey'' key xs = foldr (\(k, v) acc -> if key == k then Just v else acc) Nothing xs
