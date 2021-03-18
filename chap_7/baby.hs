b45d31d35cf8a5c43f18ba0e562ac2e3ba05027c
Akira Yamamoto <ak-yamamoto@k-bokka.com>
2021-03-18 23:00:08 +0900

    Add area func using Shape

--- chap_7/baby.hs
+++ chap_7/baby.hs
@@ -2,3 +2,7 @@
 
 data Shape = Circle Float Float Float |
              Rectangle Float Float Float Float
+
+area :: Shape -> Float
+area (Circle _ _ r) = pi * r ^ (2 :: Integer)
+area (Rectangle x1 y1 x2 y2) = (abs $ x2 - x1) * (abs $ y2 -y1)
