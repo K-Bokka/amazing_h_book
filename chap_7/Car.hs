6d0f5fffe8237abaab45250b590e9de374ec2d02
Akira Yamamoto <ak-yamamoto@k-bokka.com>
2021-03-20 22:02:55 +0900

    Add tellCar func

--- chap_7/Car.hs
+++ chap_7/Car.hs
@@ -9,3 +9,11 @@ data Car' a b c = Car' { company' :: a
                        , model' :: b
                        , year' :: c
                        } deriving Show
+
+tellCar :: Car -> String
+tellCar (Car {company = c, model = m, year = y}) =
+  "This " ++ c ++ " " ++ m ++ " was made in " ++ show y
+
+tellCar' :: (Show a) => Car' String String a -> String
+tellCar' (Car' {company' = c, model' = m, year' = y}) =
+  "This " ++ c ++ " " ++ m ++ " was made in " ++ show y
