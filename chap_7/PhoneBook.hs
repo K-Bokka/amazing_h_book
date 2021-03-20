f1d8229437c1deb0aa9c6218ffb91ba3be06c3b4
Akira Yamamoto <ak-yamamoto@k-bokka.com>
2021-03-20 22:48:48 +0900

    Add inPhoneBook func

--- chap_7/PhoneBook.hs
+++ chap_7/PhoneBook.hs
@@ -15,3 +15,9 @@ type PhoneNumber = String
 type Name = String
 
 type PhoneBook = [(Name, PhoneNumber)]
+
+inPhoneBook :: Name -> PhoneNumber -> PhoneBook -> Bool
+inPhoneBook name number book = elem (name, number) book
+
+inPhoneBook' :: String -> String -> [(String, String)] -> Bool
+inPhoneBook' name number book = elem (name, number) book
