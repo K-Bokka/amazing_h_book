33f15c84890a526be79b6176aa69de84c37039b1
Akira Yamamoto <ak-yamamoto@k-bokka.com>
2021-03-20 21:54:55 +0900

    Add Car' to using type parameter

--- chap_7/Car.hs
+++ chap_7/Car.hs
@@ -4,3 +4,8 @@ data Car = Car { company :: String
                , model :: String
                , year :: Int
                } deriving Show
+
+data Car' a b c = Car' { company' :: a
+                       , model' :: b
+                       , year' :: c
+                       } deriving Show
