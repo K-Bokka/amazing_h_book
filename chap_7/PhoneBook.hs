2ac647ab3776694760f3af38649813ca500d2b76
Akira Yamamoto <ak-yamamoto@k-bokka.com>
2021-03-20 22:43:48 +0900

    Add type PhoneBook

---
+++ chap_7/PhoneBook.hs
@@ -0,0 +1,14 @@
+{-# OPTIONS -Wall -Werror #-}
+
+phoneBook :: [(String, String)]
+phoneBook =
+    [("betty", "555-2938")
+    ,("bonnie", "452-2928")
+    ,("patsy", "493-2928")
+    ,("lucille", "205-2928")
+    ,("wendy", "939-8282")
+    ,("penny", "853-2492")
+    ]
+
+type PhoneBook = [(String, String)]
+
