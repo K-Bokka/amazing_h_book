4f264533b69088b40e265a041103bbef8756c45d
Akira Yamamoto <ak-yamamoto@k-bokka.com>
2021-03-20 21:33:13 +0900

    Add parameter functions

--- chap_7/Person.hs
+++ chap_7/Person.hs
@@ -1,3 +1,21 @@
 {-# OPTIONS -Wall -Werror #-}
 
 data Person = Person String String Int Float String String deriving Show
+
+firstName :: Person -> String
+firstName (Person fn _ _ _ _ _) = fn
+
+lastName :: Person -> String
+lastName (Person _ ln _ _ _ _) = ln
+
+age :: Person -> Int
+age (Person _ _ a _ _ _) = a
+
+height :: Person -> Float
+height (Person _ _ _ h _ _) = h
+
+phoneNumber :: Person -> String
+phoneNumber (Person _ _ _ _ pn _) = pn
+
+flavor :: Person -> String
+flavor (Person _ _ _ _ _ f) = f
