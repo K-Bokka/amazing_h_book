ee795c899c5529e4332a93bf9849b8e4d4075074
Akira Yamamoto <ak-yamamoto@k-bokka.com>
2021-03-21 16:28:25 +0900

    Add Tree type constructor

---
+++ chap_7/Tree.hs
@@ -0,0 +1,14 @@
+{-# OPTIONS -Wall -Werror #-}
+
+data Tree a = EmptyTree | Node a (Tree a) (Tree a) deriving (Show)
+
+singleton :: a -> Tree a
+singleton x = Node x EmptyTree EmptyTree
+
+treeInsert :: (Ord a) => a -> Tree a -> Tree a
+treeInsert x EmptyTree = singleton x
+treeInsert x (Node a left right)
+    | x == a = Node x left right
+    | x < a  = Node a (treeInsert x left) right
+    | x > a  = Node a left (treeInsert x right)
+treeInsert _ _ = error "The second argument must be a Tree type."
