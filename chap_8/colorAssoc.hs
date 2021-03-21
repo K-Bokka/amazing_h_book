d943615e40c91dec519cfebdb7a8f86a2573164f
Akira Yamamoto <ak-yamamoto@k-bokka.com>
2021-03-21 22:56:05 +0900

    Refactor colorAssoc

--- chap_8/colorAssoc.hs
+++ chap_8/colorAssoc.hs
@@ -6,7 +6,6 @@ main :: IO ()
 main = do
     colors <- forM ([1, 2, 3, 4] :: [Int]) $ \a -> do
         putStrLn $ "Which color do you associate with the number " ++ show a ++ "?"
-        color <- getLine
-        return color
+        getLine
     putStrLn "The colors that you associate with 1, 2, 3 and 4 are: "
     mapM_ putStrLn colors
