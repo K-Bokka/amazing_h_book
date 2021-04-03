da03b37de8b44f8205dc9fbd562928200f858cba
Akira Yamamoto <ak-yamamoto@k-bokka.com>
2021-04-03 11:10:13 +0900

    Add argments test

---
+++ chap_9/arg-test.hs
@@ -0,0 +1,12 @@
+{-# OPTIONS -Wall -Werror #-}
+
+import System.Environment
+
+main :: IO ()
+main = do
+    args <- getArgs
+    progName <- getProgName
+    putStrLn "The arguments are:"
+    mapM_ putStrLn args
+    putStrLn "The program name is:"
+    putStrLn progName
