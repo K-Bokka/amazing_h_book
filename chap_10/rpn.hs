8fcf99617292fc4647e7e354f97e9016345f7689
Akira Yamamoto <ak-yamamoto@k-bokka.com>
2021-04-04 12:42:05 +0900

    Add Reverse Polish Notation func

---
+++ chap_10/rpn.hs
@@ -0,0 +1,8 @@
+{-# OPTIONS -Wall -Werror #-}
+
+solveRPN :: String -> Double
+solveRPN expression = head . foldl foldingFunction [] $ words expression
+    where foldingFunction (x:y:ys) "*" = (y * x):ys
+          foldingFunction (x:y:ys) "+" = (y + x):ys
+          foldingFunction (x:y:ys) "-" = (y - x):ys
+          foldingFunction xs numberString  = (read numberString):xs
