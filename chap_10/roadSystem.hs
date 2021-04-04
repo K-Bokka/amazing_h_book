805f79c9d38626f86fefc942e5b514b3cfe04e2d
Akira Yamamoto <ak-yamamoto@k-bokka.com>
2021-04-04 13:42:48 +0900

    Fix roadSystem as command

--- chap_10/roadSystem.hs
+++ chap_10/roadSystem.hs
@@ -4,13 +4,6 @@ data Section = Section { getA :: Int, getB :: Int, getC :: Int } deriving Show
 
 type RoadSystem = [Section]
 
-heathrowToLondon :: RoadSystem
-heathrowToLondon = [ Section 50 10 30
-                   , Section 5 90 20
-                   , Section 40 2 25
-                   , Section 10 8 0
-                   ]
-
 data Label = A | B | C deriving Show
 type Path = [(Label, Int)]
 
@@ -41,3 +34,14 @@ groupsOf :: Int -> [a] -> [[a]]
 groupsOf 0 _ = undefined
 groupsOf _ [] = []
 groupsOf n xs = take n xs : groupsOf n (drop n xs)
+
+main :: IO ()
+main = do
+    contents <- getContents
+    let threes = groupsOf 3 (map read $ lines contents)
+        roadSystem = map (\[a, b, c] -> Section a b c) threes
+        path = optimalPath roadSystem
+        pathString = concat $ map (show . fst) path
+        pathTime = sum $ map snd path
+    putStrLn $ "The best path to take is: " ++ pathString
+    putStrLn $ "Time taken: " ++ (show pathTime)
