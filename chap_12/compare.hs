592f330347c4990e6d82a677c09e2e6c68307031
Akira Yamamoto <ak-yamamoto@k-bokka.com>
2021-04-10 12:53:56 +0900

    Add lightCompare' func using Monoid

--- chap_12/compare.hs
+++ chap_12/compare.hs
@@ -4,3 +4,6 @@ lightCompare :: String -> String -> Ordering
 lightCompare x y = let a = length x `compare` length y
                        b = x `compare` y
                    in if a == EQ then b else a
+
+lightCompare' :: String -> String -> Ordering
+lightCompare' x y = (length x `compare` length y) `mappend` (x `compare` y)
