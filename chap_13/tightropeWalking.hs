0b9fe6c380f0e7ce7657dcae14b7a7c39bf56847
Akira Yamamoto <ak-yamamoto@k-bokka.com>
2021-04-11 13:39:05 +0900

    Change landRight left to return Maybe Pole

--- chap_13/tightropeWalking.hs
+++ chap_13/tightropeWalking.hs
@@ -11,3 +11,13 @@ landRight n (left, right) = (left, right + n)
 
 (-:) :: a -> (a -> b) -> b
 x -: f = f x
+
+landLeft' :: Birds -> Pole -> Maybe Pole
+landLeft' n (left, right)
+    | abs ((left + n) - right) < 4 = Just (left + n, right)
+    | otherwise = Nothing
+
+landRight' :: Birds -> Pole -> Maybe Pole
+landRight' n (left, right)
+    | abs (left - (right + n)) < 4 = Just (left, right + n)
+    | otherwise = Nothing
