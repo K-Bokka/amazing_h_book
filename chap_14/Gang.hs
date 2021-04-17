2d03f9ad124c8024e14cb52b1197d3a82723d508
Akira Yamamoto <ak-yamamoto@k-bokka.com>
2021-04-17 09:20:05 +0900

    Add applyLog func

--- chap_14/Gang.hs
+++ chap_14/Gang.hs
@@ -5,3 +5,6 @@ isBigGang x = x > 9
 
 isBigGang' :: Int -> (Bool, String)
 isBigGang' x = (x > 9, "Compare gang size to 9")
+
+applyLog :: (a, String) -> (a -> (b, String)) -> (b, String)
+applyLog (x, preLog) f = let (y, newLog) = f x in (y, preLog ++ newLog)
