878be3f65249e9df73e98ef3a4b3cacaac87e46f
Akira Yamamoto <ak-yamamoto@k-bokka.com>
2021-04-17 09:25:00 +0900

    Add applyLog' func to use Monoid

--- chap_14/Gang.hs
+++ chap_14/Gang.hs
@@ -8,3 +8,6 @@ isBigGang' x = (x > 9, "Compare gang size to 9")
 
 applyLog :: (a, String) -> (a -> (b, String)) -> (b, String)
 applyLog (x, preLog) f = let (y, newLog) = f x in (y, preLog ++ newLog)
+
+applyLog' :: (Monoid m) => (a, m) -> (a -> (b, m)) -> (b, m)
+applyLog' (x, preLog) f = let (y, newLog) = f x in (y, preLog `mappend` newLog)
