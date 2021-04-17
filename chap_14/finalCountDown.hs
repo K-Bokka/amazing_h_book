6778106672269af1fcc76060a7b3850bfa0acf76
Akira Yamamoto <ak-yamamoto@k-bokka.com>
2021-04-17 10:52:42 +0900

    Add final count down func

---
+++ chap_14/finalCountDown.hs
@@ -0,0 +1,17 @@
+{-# OPTIONS -Wall -Werror #-}
+import DiffList
+import Control.Monad.Writer
+
+finalCountDown :: Int -> Writer (DiffList String) ()
+finalCountDown 0 = do
+    tell $ toDiffList ["0"]
+finalCountDown x = do
+    finalCountDown (x-1)
+    tell $ toDiffList [show x]
+
+finalCountDown' :: Int -> Writer [String] ()
+finalCountDown' 0 = do
+    tell ["0"]
+finalCountDown' x = do
+    finalCountDown' (x-1)
+    tell [show x]
