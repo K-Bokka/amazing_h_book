89f76d997713997b100041a2b5b82fa72839c839
Akira Yamamoto <ak-yamamoto@k-bokka.com>
2021-04-17 11:58:40 +0900

    Add threeCoins func using randomSt

--- chap_14/randomSt.hs
+++ chap_14/randomSt.hs
@@ -5,3 +5,10 @@ import Control.Monad.State
 
 randomSt :: (RandomGen g, Random a) => State g a
 randomSt = state random
+
+threeCoins :: State StdGen (Bool, Bool, Bool)
+threeCoins = do
+    a <- randomSt
+    b <- randomSt
+    c <- randomSt
+    return (a, b, c)
