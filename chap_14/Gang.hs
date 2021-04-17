f6d37f3dbaf5e7860e99e9f958f1e3bdc82f22df
Akira Yamamoto <ak-yamamoto@k-bokka.com>
2021-04-17 09:31:16 +0900

    Add addDrink func

--- chap_14/Gang.hs
+++ chap_14/Gang.hs
@@ -1,5 +1,7 @@
 {-# OPTIONS -Wall -Werror #-}
 
+import Data.Monoid
+
 isBigGang :: Int -> Bool
 isBigGang x = x > 9
 
@@ -11,3 +13,11 @@ applyLog (x, preLog) f = let (y, newLog) = f x in (y, preLog ++ newLog)
 
 applyLog' :: (Monoid m) => (a, m) -> (a -> (b, m)) -> (b, m)
 applyLog' (x, preLog) f = let (y, newLog) = f x in (y, preLog `mappend` newLog)
+
+type Food = String
+type Price = Sum Int
+
+addDrink :: Food -> (Food, Price)
+addDrink "beans" = ("milk", Sum 25)
+addDrink "herky" = ("whiskey", Sum 99)
+addDrink _ = ("Beer", Sum 30)
