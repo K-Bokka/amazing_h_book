f01a56ff84774493ac6e60505f6bacceb6e99b55
Akira Yamamoto <ak-yamamoto@k-bokka.com>
2021-04-18 12:03:19 +0900

    Refactor rpn using Maybe

---
+++ chap_14/rpn.hs
@@ -0,0 +1,22 @@
+{-# OPTIONS -Wall -Werror #-}
+
+import Control.Monad
+
+solveRPN :: String -> Maybe Double
+solveRPN st = do
+     [result] <- foldM foldingFunction [] $ words st
+     return result
+
+foldingFunction :: [Double] -> String -> Maybe [Double]
+foldingFunction (x:y:ys) "*" = return $ (y * x):ys
+foldingFunction (x:y:ys) "+" = return $ (y + x):ys
+foldingFunction (x:y:ys) "-" = return $ (y - x):ys
+foldingFunction (x:y:ys) "/" = return $ (y / x):ys
+foldingFunction (x:y:ys) "^" = return $ (y ** x):ys
+foldingFunction (x:xs) "ln" = return $ log x:xs
+foldingFunction xs "sum" = return [sum xs]
+foldingFunction xs numberString = liftM (:xs) $ readMaybe numberString
+
+readMaybe :: (Read a) => String -> Maybe a
+readMaybe st = case reads st of [(x, "")] -> Just x
+                                _         -> Nothing
