7d206cca24233ae298d160962cd6a9077c4f57f1
Akira Yamamoto <ak-yamamoto@k-bokka.com>
2021-04-18 12:42:33 +0900

    Add flatten func for Prob

--- chap_14/Prob.hs
+++ chap_14/Prob.hs
@@ -1,6 +1,18 @@
 {-# OPTIONS -Wall -Werror #-}
 
+import Data.Ratio
+
 newtype Prob a = Prob { getProb :: [(a, Rational)] } deriving Show
 
 instance Functor Prob where
     fmap f (Prob xs) = Prob $ map (\(x, p) -> (f x, p)) xs
+
+thisSituation :: Prob (Prob Char)
+thisSituation = Prob
+    [(Prob [('a', 1%2), ('b', 1%2)], 1%4)
+    ,(Prob [('c', 1%2), ('d', 1%2)], 3%4)
+    ]
+
+flatten :: Prob (Prob a) -> Prob a
+flatten (Prob xs) = Prob $ concat $ map multiAll xs
+    where multiAll (Prob ixs, p) = map (\(x, r) -> (x, p*r)) ixs
