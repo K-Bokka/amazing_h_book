766f9cbe5538a1b5a4a64a3971d88fae9da00ef1
Akira Yamamoto <ak-yamamoto@k-bokka.com>
2021-04-18 12:17:27 +0900

    Add generic func to knight journey

--- chap_14/knightJourney.hs
+++ chap_14/knightJourney.hs
@@ -29,3 +29,9 @@ in3' start = return start >>= moveKnight >>= moveKnight >>= moveKnight
 
 canReachIn3 :: KnightPos -> KnightPos -> Bool
 canReachIn3 start end = elem end $ in3 start
+
+inMany :: Int -> KnightPos -> [KnightPos]
+inMany x start = return start >>= foldr (<=<) return (replicate x moveKnight)
+
+canReachIn :: Int -> KnightPos -> KnightPos -> Bool
+canReachIn x start end = elem end $ inMany x start
