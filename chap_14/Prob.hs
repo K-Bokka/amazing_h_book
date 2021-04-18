08eec45747edb963fbcbed1d0a0f780ed66121d3
Akira Yamamoto <ak-yamamoto@k-bokka.com>
2021-04-18 14:08:50 +0900

    Prob is Monad

--- chap_14/Prob.hs
+++ chap_14/Prob.hs
@@ -11,6 +11,9 @@ instance Applicative Prob where
     pure x = Prob [(x, 1%1)]
     (Prob fs) <*> (Prob xs) = Prob [(f x, r) | (f, _) <- fs, (x, r) <- xs]
 
+instance Monad Prob where
+    m >>= f = flatten (fmap f m)
+
 thisSituation :: Prob (Prob Char)
 thisSituation = Prob
     [(Prob [('a', 1%2), ('b', 1%2)], 1%4)
