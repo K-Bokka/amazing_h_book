a1c825a85c60d01c0167aad3338bb97783712921
Akira Yamamoto <ak-yamamoto@k-bokka.com>
2021-04-18 12:34:00 +0900

    Prob is Fucntor

--- chap_14/Prob.hs
+++ chap_14/Prob.hs
@@ -1,3 +1,6 @@
 {-# OPTIONS -Wall -Werror #-}
 
 newtype Prob a = Prob { getProb :: [(a, Rational)] } deriving Show
+
+instance Functor Prob where
+    fmap f (Prob xs) = Prob $ map (\(x, p) -> (f x, p)) xs
