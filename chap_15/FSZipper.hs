8b4adbb93ebd048cfc0681e19e05399bf7440e0d
Akira Yamamoto <ak-yamamoto@k-bokka.com>
2021-04-24 13:01:41 +0900

    Fix FSZipper functions

--- chap_15/FSZipper.hs
+++ chap_15/FSZipper.hs
@@ -5,3 +5,19 @@ import FileSystem
 data FSCrumb = FSCrumb Name [FSItem] [FSItem] deriving Show
 type FSZipper = (FSItem, [FSCrumb])
 
+(-:) :: a -> (a -> b) -> b
+x -: f = f x
+
+fsUp :: FSZipper -> FSZipper
+fsUp (item, (FSCrumb name ls rs):bs) = (Folder name (ls ++ [item] ++ rs), bs)
+fsUp (_, []) = error "Top!"
+
+nameIs :: Name -> FSItem -> Bool
+nameIs name (Folder folderName _) = name == folderName
+nameIs name (File fileName _) = name == fileName
+
+fsTo :: Name -> FSZipper -> FSZipper
+fsTo name (Folder folderName items, bs) =
+    let (ls, item:rs) = break (nameIs name) items
+    in  (item, FSCrumb folderName ls rs:bs)
+fsTo _ _ = error "Bottom!"
