fcad0bc70b67636667eee834f78b88de3ccc0bf2
Akira Yamamoto <ak-yamamoto@k-bokka.com>
2021-04-24 11:39:28 +0900

    Add goUp func

--- chap_15/Crumb.hs
+++ chap_15/Crumb.hs
@@ -13,3 +13,9 @@ goLeft (Empty, _) = error "It's a dead end!"
 goRight :: (Tree a, Breadcrumbs a) -> (Tree a, Breadcrumbs a)
 goRight (Node x l r, bs) = (r, RightCrumb x l:bs)
 goRight (Empty, _) = error "It's a dead end!"
+
+goUp :: (Tree a, Breadcrumbs a) -> (Tree a, Breadcrumbs a)
+goUp (t, LeftCrumb x r:bs) = (Node x t r, bs)
+goUp (t, RightCrumb x l:bs) = (Node x l t, bs)
+goUp (Empty, _) = error "It's at the top of the tree!"
+goUp (_, []) = error "Didn't drop the breadcrumbs!"
