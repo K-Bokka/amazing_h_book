bfa5eb23ae12c8e260640dfd0fbb96c8cb7f672a
Akira Yamamoto <ak-yamamoto@k-bokka.com>
2021-04-24 11:00:03 +0900

    Add changeToP' func using Direction data

--- chap_15/Tree.hs
+++ chap_15/Tree.hs
@@ -6,6 +6,15 @@ changeToP :: Tree Char -> Tree Char
 changeToP (Node x l (Node y (Node _ m n) r)) = (Node x l (Node y (Node 'P' m n) r))
 changeToP _ = error "Oops!"
 
+data Direction = L | R deriving (Show)
+type Directions = [Direction]
+
+changeToP' :: Directions -> Tree Char -> Tree Char
+changeToP' (L:ds) (Node x l r) = Node x (changeToP' ds l) r
+changeToP' (R:ds) (Node x l r) = Node x l (changeToP' ds r)
+changeToP' [] (Node _ l r) = Node 'P' l r
+changeToP' _ Empty = error "It's a dead end!"
+
 freeTree :: Tree Char
 freeTree =
     Node 'P'
